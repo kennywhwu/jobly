671d8b435e0491caead08a7933545055710d1b4a
Kenny Hwu <kennywhwu@gmail.com>
2018-11-07 10:27:24 -0800

    added JoblyApi and Routes components

---
+++ src/JoblyApi.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+const BASE_URL = 'http://localhost:3001';
+
+class JoblyApi {
+  static async request(endpoint, params = {}, verb = 'get') {
+    // for now, hardcode a token for user "testuser"
+    let _token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc' +
+      '3R1c2VyIiwiaXNfYWRtaW4iOmZhbHNlLCJpYXQiOjE1NDE1NjQ2Nzl9.LYDHSkl81gEm' +
+      '7jfHv9wJhzD4ndpuBkSzBan8Nirb6UY';
+
+    console.debug('API Call:', endpoint, params, verb);
+
+    let q;
+
+    if (verb === 'get') {
+      q = axios.get(`${BASE_URL}/${endpoint}`, {
+        params: { _token, ...params }
+      });
+    } else if (verb === 'post') {
+      q = axios.post(`${BASE_URL}/${endpoint}`, { _token, ...params });
+    } else if (verb === 'patch') {
+      q = axios.patch(`${BASE_URL}/${endpoint}`, { _token, ...params });
+    }
+
+    try {
+      return (await q).data;
+    } catch (err) {
+      console.error('API Error:', err.response);
+      let message = err.response.data.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  static async getCompany(handle) {
+    let res = await this.request(`companies/${handle}`);
+    return res.company;
+  }
+}
+
+export default JoblyApi;
