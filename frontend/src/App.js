3f0cdb0f104b95e5fd5ad906fb2ceffd206b4c33
Kenny Hwu <kennywhwu@gmail.com>
2018-11-09 11:31:15 -0800

    refactored logout alert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,9 +44,13 @@ class App extends Component {
   }
 
   clearUser() {
-    this.setState({ currentUser: null }, () =>
-      localStorage.removeItem('token')
-    );
+    this.setState({ currentUser: null }, () => {
+      let token = localStorage.getItem('token');
+      localStorage.removeItem('token');
+      if (token) {
+        this.props.triggerAlert('success', 'Logged out!');
+      }
+    });
   }
 
   triggerAlert(type, message) {
